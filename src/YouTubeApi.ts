24b894714e636dfb48c552f5a131e8027b93cc93
KanaSakaguchi <sakaguchi@sencorp.co.jp>
2020-01-12 12:33:05 +0900

    検索結果を表示

--- src/YouTubeApi.ts
+++ src/YouTubeApi.ts
@@ -1,10 +1,12 @@
 import SearchListResponse = GoogleAppsScript.YouTube.Schema.SearchListResponse
+import SearchResult = GoogleAppsScript.YouTube.Schema.SearchResult
+import { extractItemResource_, ItemResource } from './extractItemResource'
 
-function search(keyword: string) {
+function search(keyword: string): ItemResource[] {
   const result: SearchListResponse = YouTube.Search.list('id,snippet',
     {
       q: keyword
     })
 
-  return result
+  return result.items.map((video: SearchResult): ItemResource => extractItemResource_(video))
 }
---
+++ src/extractItemResource.ts
@@ -0,0 +1,29 @@
+import SearchResult = GoogleAppsScript.YouTube.Schema.SearchResult
+
+export interface ItemResource {
+  url: string,
+  title: string,
+  thumbnailUrl: string,
+  channelName: string,
+  channelUrl: string
+}
+
+export function extractItemResource_(video: SearchResult): ItemResource {
+  let itemResource: ItemResource = {
+    url: '',
+    title: video.snippet.title,
+    thumbnailUrl: video.snippet.thumbnails.default.url,
+    channelName: video.snippet.channelTitle,
+    channelUrl: `https://www.youtube.com/channel/${video.snippet.channelId}`
+  }
+
+  if (video.id.videoId) {
+    itemResource.url = `https://www.youtube.com/watch?v=${video.id.videoId}`
+  } else if (video.id.playlistId) {
+    itemResource.url = `https://www.youtube.com/playlist?list=${video.id.playlistId}`
+  } else if (video.id.channelId) {
+    itemResource.url = `https://www.youtube.com/channel/${video.id.channelId}`
+  }
+
+  return itemResource
+}
