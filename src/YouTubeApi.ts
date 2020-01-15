a9f1d1047dc4b90d78e4eada433c874eb902af8e
KanaSakaguchi <sakaguchi@sencorp.co.jp>
2020-01-15 21:35:24 +0900

    動画の長さを表示

--- src/YouTubeApi.ts
+++ src/YouTubeApi.ts
@@ -1,6 +1,6 @@
 import SearchListResponse = GoogleAppsScript.YouTube.Schema.SearchListResponse
 import SearchResult = GoogleAppsScript.YouTube.Schema.SearchResult
-import { extractItemResource_, ItemResource } from './extractItemResource'
+import { extractItemResource_, extractVideoResource_, ItemResource } from './extractItemResource'
 
 function search(keyword: string): ItemResource[] {
   const result: SearchListResponse = YouTube.Search.list('id,snippet',
@@ -10,4 +10,15 @@ function search(keyword: string): ItemResource[] {
     })
 
   return result.items.map((video: SearchResult): ItemResource => extractItemResource_(video))
+}
+
+function getVideoResource(videoId: string): ItemResource {
+  const video = YouTube.Videos.list('id,snippet,contentDetails,status', {
+    id: videoId
+  }).items[0]
+  if (!video) {
+    throw new Error('Failed to get video resource.')
+  }
+
+  return extractVideoResource_(video)
 }
--- src/extractItemResource.ts
+++ src/extractItemResource.ts
@@ -1,27 +1,97 @@
 import SearchResult = GoogleAppsScript.YouTube.Schema.SearchResult
+import Video = GoogleAppsScript.YouTube.Schema.Video
+
+enum ItemType {
+  VIDEO = 'video',
+  PLAY_LIST = 'playlist'
+}
 
 export interface ItemResource {
+  id: string,
+  type: ItemType,
   url: string,
   title: string,
   thumbnailUrl: string,
+  videoLength: string,
   channelName: string,
   channelUrl: string
 }
 
 export function extractItemResource_(video: SearchResult): ItemResource {
   let itemResource: ItemResource = {
+    id: '',
+    type: ItemType.VIDEO,
     url: '',
     title: video.snippet.title,
     thumbnailUrl: video.snippet.thumbnails.default.url,
+    videoLength: '読み込み中',
     channelName: video.snippet.channelTitle,
     channelUrl: `https://www.youtube.com/channel/${video.snippet.channelId}`
   }
 
   if (video.id.videoId) {
-    itemResource.url = `https://www.youtube.com/watch?v=${video.id.videoId}`
+    itemResource.id = video.id.videoId
+    itemResource.url = `https://www.youtube.com/watch?v=${itemResource.id}`
   } else if (video.id.playlistId) {
-    itemResource.url = `https://www.youtube.com/playlist?list=${video.id.playlistId}`
+    itemResource.id = video.id.playlistId
+    itemResource.type = ItemType.PLAY_LIST
+    itemResource.url = `https://www.youtube.com/playlist?list=${itemResource.id}`
   }
 
   return itemResource
+}
+
+export function extractVideoResource_(video: Video): ItemResource {
+  return {
+    id: video.id,
+    type: ItemType.VIDEO,
+    url: `https://www.youtube.com/watch?v=${video.id}`,
+    title: video.snippet.title,
+    thumbnailUrl: video.snippet.thumbnails.default.url,
+    videoLength: exchangeTime_(video.contentDetails.duration),
+    channelName: video.snippet.channelTitle,
+    channelUrl: `https://www.youtube.com/channel/${video.snippet.channelId}`
+  }
+}
+
+function exchangeTime_(duration: string): string {
+  let time: RegExpMatchArray
+
+  time = duration.match(/PT(\d+)H(\d+)M(\d+)S/)
+  if (time) {
+    time.shift()
+    return Utilities.formatString('%d:%02d:%02d', ...time)
+  }
+
+  time = duration.match(/PT(\d+)M(\d+)S/)
+  if (time) {
+    time.shift()
+    return Utilities.formatString('%d:%02d', ...time)
+  }
+
+  time = duration.match(/PT(\d+)S/)
+  if (time) {
+    time.shift()
+    return Utilities.formatString('0:%02d', ...time)
+  }
+
+  time = duration.match(/PT(\d+)H(\d+)S/)
+  if (time) {
+    time.shift()
+    return Utilities.formatString('%d:00:%02d', ...time)
+  }
+
+  time = duration.match(/PT(\d+)H/)
+  if (time) {
+    time.shift()
+    return Utilities.formatString('%d:00:00', ...time)
+  }
+
+  time = duration.match(/PT(\d+)M/)
+  if (time) {
+    time.shift()
+    return Utilities.formatString('%d:00', ...time)
+  }
+
+  return duration
 }
