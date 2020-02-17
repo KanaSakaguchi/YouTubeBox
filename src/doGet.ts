9a01322d01f9346a2f4cf58d482ce66ec8495d4b
KanaSakaguchi <sakaguchi@sencorp.co.jp>
2020-02-17 22:42:06 +0900

    パスによって再生画面に行けるようにする

--- src/doGet.ts
+++ src/doGet.ts
@@ -1,3 +1,8 @@
-function doGet () {
-  return HtmlService.createHtmlOutputFromFile('src/order').setTitle('YouTube Box - order')
+function doGet (data) {
+  switch (data.pathInfo) {
+    case 'player':
+      return HtmlService.createHtmlOutputFromFile('src/player').setTitle('YouTube Box - Player')
+    case 'order':
+      return HtmlService.createHtmlOutputFromFile('src/order').setTitle('YouTube Box - Order')
+  }
 }
